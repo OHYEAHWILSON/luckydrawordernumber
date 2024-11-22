9cf0350c4f6bf69f6e2615e75841b2a2c2bdbe27
Wilson <wilson.liu@ametagroup.com>
2024-11-22 17:01:01 -0500

    Updated server.js to use Firebase credentials from environment variables

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/record-draw-result', async (req, res) => {
 // Set up the port for the server to listen on
 const PORT = process.env.PORT || 5015;  // Ensure using dynamic port assignment
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Fetch draw results
